f48de07eeb68052ee9ad9f3f066ad9d6c4a5a085
Mees Vermeulen <mees@vermeulen.me>
2020-05-01 11:52:05 +0200

    Sanitize output from djvutxt, as it may contain invalid utf-8.

--- src/adapters/djvutxt.rs
+++ src/adapters/djvutxt.rs
@@ -47,9 +47,14 @@ impl SpawningFileAdapter for DjvutxtAdapter {
     }
 
     fn postproc(line_prefix: &str, inp: &mut dyn Read, oup: &mut dyn Write) -> Fallible<()> {
+        // String returned from djvutxt may return invalid utf8, sanitize it first.
+        let mut vec = Vec::<u8>::new();
+        inp.read_to_end(&mut vec)?;
+        let sanitized_string = String::from_utf8_lossy(&vec).into_owned();
+
         // prepend Page X to each line
         let mut page = 1;
-        for line in BufReader::new(inp).lines() {
+        for line in BufReader::new(sanitized_string.as_bytes()).lines() {
             let mut line = line?;
             if line.contains('\x0c') {
                 // page break
